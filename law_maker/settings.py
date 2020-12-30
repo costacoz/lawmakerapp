fee86ea3501635f616628589859a8249cfc03c4c
Konstantin <buserandi@gmail.com>
2020-12-30 12:26:42 +0200

    Added separate settings file and gitignored it.

--- law_maker/settings.py
+++ law_maker/settings.py
@@ -19,7 +19,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+# SECRET_KEY is in my_settings.py file, not included into this project
+# use get_random_secret_key() to generate your own key and insert it here
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
